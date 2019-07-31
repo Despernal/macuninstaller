29a784925c741de153d1b35e6ca60a4b1e3fbc6e
Rémi Héneault <remi@heneault.fr>
2019-07-31 15:59:10 +0200

    ...

--- purge_app.py
+++ purge_app.py
@@ -9,6 +9,14 @@ import plistlib
 from pathlib import Path
 
 
+SEARCH_DIRECTORIES = {
+    Path.home() / "Library",
+    "/Library",
+    "/System/Library",
+    "/var"
+}
+
+
 def read_plist(app_path):
     """This function returns a set containing the informations of an app."""
 
@@ -16,7 +24,7 @@ def read_plist(app_path):
         plist_path = Path(app_path, "Contents/Info.plist").resolve(strict=True)
     except FileNotFoundError:
         # The name of the app
-        return [plist_path.parents[1].stem]
+        return {plist_path.parents[1].stem}
 
     plist_content = plistlib.loads(plist_path.read_bytes())
 
@@ -31,31 +39,46 @@ def read_plist(app_path):
     return relevant_infos
 
 
-def scan(path_to_app):
-
-    hints = read_plist(path_to_app)
-    print("[*] Found following hints : {}".format(hints))
-
-    # Usual directories for app-related files
-    search_directories = {
-        Path.home() / "Library",
-        "/Library",
-        "/System/Library",
-        "/var"
-    }
+def path_walk(start_path, hints):
+    """Equivalent to `os.walk` using pathlib."""
 
     def check_hints(str_path):
         return any(h in str_path for h in hints)
 
-    for directory in search_directories:
+    names = start_path.iterdir()
+
+    dirs = (node for node in names if node.is_dir() is True)
+    nondirs = (node for node in names if node.is_dir() is False)
+
+    for name in dirs:
+        if check_hints(str(name)):
+            yield name
+
+        elif not name.is_symlink():
+            for x in path_walk(name, hints):
+                yield x
+
+    for name in nondirs:
+        if check_hints(str(name)):
+            yield name
+
+
+def scan(path_to_app):
+
+    hints = read_plist(path_to_app)
+
+    results = set()
+
+    for directory in SEARCH_DIRECTORIES:
         print("[i] Searching in {}...".format(directory))
 
         # REVIEW: Not efficient
-        subdirs = Path(directory).glob("**")
+        subdirs = Path(directory).iterdir()
 
         for dir in subdirs:
+
             relative_path = str(dir.relative_to(directory))
 
             if check_hints(relative_path):
-                print("  - {}".format(relative_path))
+                results.add(dir)
                 # relative_path.rmdir()
