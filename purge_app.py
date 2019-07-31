1508b4a49c53052098a179f1afd8d11f1d6932e0
Rémi Héneault <remi@heneault.fr>
2019-07-31 17:11:39 +0200

    Permission errors management

--- purge_app.py
+++ purge_app.py
@@ -11,9 +11,9 @@ from pathlib import Path
 
 SEARCH_DIRECTORIES = {
     Path.home() / "Library",
-    "/Library",
-    "/System/Library",
-    "/var"
+    Path("/Library"),
+    Path("/System/Library"),
+    Path("/var")
 }
 
 
@@ -39,46 +39,43 @@ def read_plist(app_path):
     return relevant_infos
 
 
+def check_hints(path_name, hints):
+    return any(h in str(path_name) for h in hints)
+
+
 def path_walk(start_path, hints):
     """Equivalent to `os.walk` using pathlib."""
 
-    def check_hints(path_name):
-        return any(h in str(path_name) for h in hints)
-
-    names = list(start_path.iterdir())
+    try:
+        names = list(start_path.iterdir())
+    except PermissionError:
+        return
 
     dirs = (node for node in names if node.is_dir())
     nondirs = (node for node in names if node.is_file())
 
     for name in dirs:
-        if check_hints(name):
+
+        if check_hints(name, hints):
             yield name
 
         elif not name.is_symlink():
-            for next in path_walk(name, hints):
-                yield next
+            yield from path_walk(name, hints)
 
     for name in nondirs:
-        if check_hints(name):
+        if check_hints(name, hints):
             yield name
 
 
 def scan(path_to_app):
 
     hints = read_plist(path_to_app)
-
     results = set()
 
     for directory in SEARCH_DIRECTORIES:
         print("[i] Searching in {}...".format(directory))
 
-        # REVIEW: Not efficient
-        subdirs = Path(directory).iterdir()
+        for match in path_walk(directory, hints):
+            results.add(match)
 
-        for dir in subdirs:
-
-            relative_path = str(dir.relative_to(directory))
-
-            if check_hints(relative_path):
-                results.add(dir)
-                # relative_path.rmdir()
+    return results
